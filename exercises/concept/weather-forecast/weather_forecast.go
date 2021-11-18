46b87c49a2aa079f259a4bd9f8ec3f7b2d458da4
bjwangsonglin <wangsonglin@jd.com>
2021-11-18 17:34:21 +0800

    learn how to comment package  variable and method

--- exercises/concept/weather-forecast/weather_forecast.go
+++ exercises/concept/weather-forecast/weather_forecast.go
@@ -1,8 +1,14 @@
+// Package weather : weather management.
+// can forecast the weather.
 package weather
 
+//CurrentCondition: the weather condition.
 var CurrentCondition string
+
+//CurrentLocation: the location.
 var CurrentLocation string
 
+// Forecast : forecast the weather condition in the city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
