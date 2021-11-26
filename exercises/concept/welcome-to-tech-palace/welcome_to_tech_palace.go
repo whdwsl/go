be54e1db84998d42170838e264fbd11772415296
bjwangsonglin <wangsonglin@jd.com>
2021-11-26 15:22:33 +0800

    strings util

--- exercises/concept/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ exercises/concept/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,34 @@
 package techpalace
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	ret := fmt.Sprint("Welcome to the Tech Palace, ", strings.ToUpper(customer))
+	return ret
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var stars bytes.Buffer
+	for i := 0; i < numStarsPerLine; i++ {
+		stars.WriteString("*")
+	}
+	content := []string{stars.String(), welcomeMsg, stars.String()}
+	ret := strings.Join(content, "\n")
+
+	return ret
+
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	ret := strings.ReplaceAll(oldMsg, "*", "")
+
+	ret = strings.TrimSpace(ret)
+	return ret
 }
